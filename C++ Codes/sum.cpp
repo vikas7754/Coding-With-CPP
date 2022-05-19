4a4d6a14801e897e5a37c9000eec15dff48a17ee
Vikas <vikask4590@gmail.com>
2022-05-20 01:37:21 +0530

    init

---
+++ C++ Codes/sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y,sum;
+    cout<<"Enter First Number: ";
+    cin>>x;
+    cout<<"Enter Second Number: ";
+    cin>>y;
+    sum = x+y;
+    cout<<"Your Sum is: "<<sum<<endl;
+    return 0;
+}
---
+++ C++ Codes/take-input.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n;
+    char c;
+    string s;
+    cout<<"Enter Number: ";
+    cin>>n;
+    cout<<"Enter Character: ";
+    cin>>c;
+    cout<<"Enter String: ";
+    cin>>s;
+    
+    cout<<"Your Number is: "<<n<<endl;
+    cout<<"Your Character is: "<<c<<endl;
+    cout<<"Your String is: "<<s<<endl;
+    return 0;
+}
